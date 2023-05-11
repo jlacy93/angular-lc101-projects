602eedd51583cc61ca293f296fd0590641c4fb4c
Josephina Lacy <josephinalacy@gmail.com>
2023-05-10 19:33:52 -0500

    bonus mission for exercise

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidatesComponent implements OnInit {
    missionName = "LaunchCode Moonshot"
-
+  
    candidates = [
     {name: 'Rusty Rutabaga', data: {age: 5, mass: '0.75 kg', sidekick: 'Blake'}, image: 'assets/images/Blake.png'},
     {name: 'Tessa Tortoise', data: {age: 126, mass: '113 kg', sidekick: 'Sally'}, image: 'assets/images/Sally.png'},
@@ -24,9 +24,17 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
+  addToCrew(person:object){
+    if(!this.crew.includes(person)){
+        this.crew.push(person)
+    };
 
+  }
 
   // BONUS: Code the changeMissionName function here:
-
+changeMissionName(name:string){
+  this.missionName = name;
+  return false;
+}
 
 }
