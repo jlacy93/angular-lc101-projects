b513b17590ccef9e1f0807de912b36eea6fdce5e
Josephina Lacy <josephinalacy@gmail.com>
2023-05-13 14:09:02 -0500

    examples completed

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   inactiveGold: boolean = false;
+   inactiveSilver: boolean = false;
+   inactiveCopper: boolean = false;
+   buttonLocation: string = "left";
 
    constructor() { }
 
    ngOnInit() { }
+
+   activateAndInactiveButtons(){
+      this.inactiveGold = !this.inactiveGold;
+      this.inactiveSilver = !this.inactiveSilver;
+      this.inactiveCopper = !this.inactiveCopper;
+   }
+
+   moveButton(oldLocation: string){
+      while(this.buttonLocation === oldLocation){
+         this.buttonLocation = ["left", "right", "center"][Math.floor(Math.random()*3)]
+      }
+      return this.buttonLocation
+   }
 
 }
